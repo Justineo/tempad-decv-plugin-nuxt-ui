ea70d5d3802649828a965702692a5b1b28fe52e3
Justineo <justice360@gmail.com>
2025-03-02 00:19:24 +0800

    fix: support icons from other vendors

--- src/components/icon.ts
+++ src/components/icon.ts
@@ -14,9 +14,16 @@ export function Icon(component: DesignComponent<IconProperties>) {
   )
 }
 
+// close -> i-lucide-close
+// simple-icons/x -> i-simple-icons-x
 export function getIconName(name?: string): string | undefined {
   if (!name) {
     return undefined
+  }
+
+  if (name.includes('/')) {
+    const [prefix, iconName] = name.split('/')
+    return `i-${prefix}-${iconName}`
   }
 
   return `i-lucide-${name}`
